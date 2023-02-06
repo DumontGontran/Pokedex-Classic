8ff34093bd6505a2fad3216ad0c0259004936a11
DumontGontran <dumont.gontran@orange.fr>
2023-02-06 02:11:51 +0100

    update pokemon navigation(navigation authorized only between 1 and 151) + add transitions

--- src/pokedexNavigation.ts
+++ src/pokedexNavigation.ts
@@ -1,26 +1,49 @@
 const createNavigationPokemon = async () => {
-    const card = <HTMLElement>document.querySelector('.pokemon-block');
-    const arrowLeft = <HTMLElement>document.createElement('i');
+    const card = document.querySelector('.pokemon-block') as HTMLElement;
+    const arrowLeft = document.createElement('i') as HTMLElement;
     arrowLeft.classList.add('fas', 'fa-arrow-circle-left');
 
-    const arrowRight = <HTMLElement>document.createElement('i');
+    const arrowRight = document.createElement('i') as HTMLElement;
     arrowRight.classList.add('fas', 'fa-arrow-circle-right');
 
     card.append(arrowLeft, arrowRight);
 
     const params = new URLSearchParams(document.location.search);
+    let id = Number(params.get('id'));
+
+    if (id === 1) {
+        arrowLeft.style.visibility = 'hidden';
+        arrowLeft.style.pointerEvents = 'none';
+    }
+    if (id === 151) {
+        arrowRight.style.visibility = 'hidden';
+        arrowRight.style.pointerEvents = 'none';
+    }
 
     arrowLeft.addEventListener('click', async (event: MouseEvent) => {
         event.preventDefault();
 
-        let id = Number(params.get('id')) - 1;
-        window.location.href = 'pokemon.html?id=' + id;
+        id = Number(params.get('id')) - 1;
+
+        if (id >= 1) {
+            card.style.opacity = '0';
+            card.style.transition = 'opacity 1000ms ease-in-out';
+            setTimeout(() => {
+                window.location.href = 'pokemon.html?id=' + id;
+            }, 1000);
+        }
     });
 
     arrowRight.addEventListener('click', async (event: MouseEvent) => {
         event.preventDefault();
 
-        let id = Number(params.get('id')) + 1;
-        window.location.href = 'pokemon.html?id=' + id;
-    }); 
+        id = Number(params.get('id')) + 1;
+        if (id <= 151) {
+            card.style.opacity = '0';
+            card.style.transition = 'opacity 1000ms ease-in-out';
+            setTimeout(() => {
+                window.location.href = 'pokemon.html?id=' + id;
+            }, 1000);
+        }
+    });
 }
