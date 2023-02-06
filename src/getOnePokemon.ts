febe96621231a57ed6b930eb79dfac6c868ff1a5
DumontGontran <dumont.gontran@orange.fr>
2023-02-06 04:38:03 +0100

    securing search by ID number in url

--- src/getOnePokemon.ts
+++ src/getOnePokemon.ts
@@ -1,11 +1,24 @@
 const fetchOnePokemon = async (): Promise<void> => {
-    const params = new URLSearchParams(document.location.search);
-    const id: number = Number(params.get('id'));
+    try {
+        const params = new URLSearchParams(document.location.search);
+        const id: number = Number(params.get('id'));
 
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
-    const data = await response.json();
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
+        const data = await response.json();
 
-    createOnePokemon({ pokemon: data });
+        if (id < 1 || id > 151) {
+            throw new Error('This pokemon doesn\'t exists in the pokedex !');
+        } else {
+            createOnePokemon({ pokemon: data });
+        }
+
+    } catch (err: any) {
+        const pokedex = document.querySelector('#pokedex') as HTMLElement;
+        const empty = document.createElement('p') as HTMLParagraphElement;
+        empty.textContent = 'This pokemon doesn\'t exists in the pokedex !';
+        empty.classList.add('pokedex-empty');
+        pokedex.append(empty);
+    }
 }
 
 const createOnePokemon = ({ pokemon }:
@@ -23,7 +36,7 @@ const createOnePokemon = ({ pokemon }:
             weight: string
         }
     }) => {
-    const pokedex = document.querySelector('#pokedex') as HTMLElement;
+    const pokedex = document.querySelector('#pokedex') as HTMLDivElement;
     const card = document.createElement('article') as HTMLElement;
     card.classList.add('pokemon-block');
 
@@ -36,27 +49,27 @@ const createOnePokemon = ({ pokemon }:
 
     spriteContainer.appendChild(sprite);
 
-    const id = document.createElement('p') as HTMLElement;
+    const id = document.createElement('p') as HTMLParagraphElement;
     id.classList.add('pokemon-id');
     id.textContent = `#${pokemon.id.toString().padStart(3, String(0))}`;
 
-    const name = document.createElement('p') as HTMLElement;
+    const name = document.createElement('p') as HTMLParagraphElement;
     name.classList.add('pokemon-name');
     name.textContent = pokemon.name;
 
-    const types = document.createElement('p') as HTMLElement;
+    const types = document.createElement('p') as HTMLParagraphElement;
     types.classList.add('pokemon-types');
     types.textContent = `Main type: ${pokemon.types[0].type.name}`;
 
-    const abilities = document.createElement('p') as HTMLElement;
+    const abilities = document.createElement('p') as HTMLParagraphElement;
     abilities.classList.add('pokemon-abilities');
     abilities.textContent = `Main ability: ${pokemon.abilities[0].ability.name}`;
 
-    const height = document.createElement('p') as HTMLElement;
+    const height = document.createElement('p') as HTMLParagraphElement;
     height.classList.add('pokemon-height');
     height.textContent = `Height: ${pokemon.height}`;
 
-    const weight = document.createElement('p') as HTMLElement;
+    const weight = document.createElement('p') as HTMLParagraphElement;
     weight.classList.add('pokemon-weight');
     weight.textContent = `Weight: ${pokemon.weight}`;
 
--- src/getOnePokemonDescription.ts
+++ src/getOnePokemonDescription.ts
@@ -1,11 +1,23 @@
 const fetchOnePokemonDescription = async (): Promise<void> => {
-    const params = new URLSearchParams(document.location.search);
-    const id: number = Number(params.get('id'));
+    try {
+        const params = new URLSearchParams(document.location.search);
+        const id: number = Number(params.get('id'));
 
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}/`)
-    const data = await response.json();
-
-    createOnePokemonDescription({ pokemonDescription: data });
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}/`)
+        const data = await response.json();
+        if (id < 1 || id > 151) {
+            throw new Error('');
+        } else {
+        createOnePokemonDescription({ pokemonDescription: data });
+        }
+    }
+    catch (err: any) {
+        const pokedex = document.querySelector('#pokedex') as HTMLElement;
+        const empty = document.createElement('p') as HTMLParagraphElement;
+        empty.textContent = '';
+        empty.classList.add('pokedex-empty');
+        pokedex.append(empty);
+    }
 }
 
 const createOnePokemonDescription = ({ pokemonDescription }:
@@ -33,21 +45,21 @@ const createOnePokemonDescription = ({ pokemonDescription }:
     }) => {
     const article = document.querySelector('article') as HTMLElement;
 
-    const description = document.createElement('p') as HTMLElement;
+    const description = document.createElement('p') as HTMLParagraphElement;
     description.classList.add('pokemon-description');
     description.textContent = `Description: This pokemon, ${pokemonDescription.flavor_text_entries[1].flavor_text}`;
     description.style.color = pokemonDescription.color.name;
 
-    const habitat = document.createElement('p') as HTMLElement;
+    const habitat = document.createElement('p') as HTMLParagraphElement;
     habitat.classList.add('pokemon-habitat');
     habitat.textContent = `Habitat: ${pokemonDescription.habitat.name}`;
     habitat.style.color = pokemonDescription.color.name;
 
-    const growthRate = document.createElement('p') as HTMLElement;
+    const growthRate = document.createElement('p') as HTMLParagraphElement;
     growthRate.classList.add('pokemon-growthrate');
     growthRate.textContent = `Growth rate: ${pokemonDescription.growth_rate.name}`;
 
-    const evolvedFrom = document.createElement('p') as HTMLElement;
+    const evolvedFrom = document.createElement('p') as HTMLParagraphElement;
     evolvedFrom.classList.add('pokemon-evolutions');
     evolvedFrom.textContent = pokemonDescription.evolves_from_species ?
         `Evolves form from: ${pokemonDescription.evolves_from_species.name}` : 'Not evolved form';
